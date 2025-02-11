9e9e9d93c39400bee74c18f6b2971f087e4c5df3
Erik Sargsyan <erosargsyan7@gmail.com>
2025-02-11 14:16:59 +0400

    feat(store): add tempWatchlistsSlice slice

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import coinsReducer from './slices/coinsSlice';
-
+import watchlistsReducer from './slices/watchlistsSlice';
+import tempWatchlistsReducer from './slices/tempWatchlistsSlice';
 export const store = configureStore({
   reducer: {
     coins: coinsReducer,
+    watchlists: watchlistsReducer,
+    tempWatchlists: tempWatchlistsReducer,
   },
 });
 
