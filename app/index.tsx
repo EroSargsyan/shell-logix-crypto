4c8c3b5c388424ee083bfeb0e8aad6a57e47c0a2
Erik Sargsyan <erosargsyan7@gmail.com>
2025-02-12 14:27:17 +0400

    feat(MainScreen): add styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,10 @@
 import { useState, useMemo, useEffect } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  SafeAreaView,
-  Button,
-  FlatList,
-  Share,
-  Pressable,
-} from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, FlatList, Share, Pressable } from 'react-native';
 import { router } from 'expo-router';
 import { useDispatch, useSelector } from 'react-redux';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { ICoin } from '@/app/types/types';
+import Colors from './constants/Colors';
 import { AppDispatch, RootState } from './redux/store';
 import {
   clearSelectedWatchlistId,
@@ -19,7 +12,6 @@ import {
   setSelectedWatchlistId,
 } from './redux/slices/watchlistsSlice';
 import WatchlistsModal from './components/ui/modal/WatchlistsModal';
-import { ICoin } from '@/app/types/types';
 
 export default function MainScreen() {
   const dispatch = useDispatch<AppDispatch>();
@@ -69,7 +61,6 @@ export default function MainScreen() {
 
     if (selectedWatchlist?.id === watchlistId) {
       const remaining = watchlists.filter((w) => w.id !== watchlistId);
-
       if (remaining.length) {
         dispatch(setSelectedWatchlistId(remaining[0].id));
       } else {
@@ -98,20 +89,26 @@ export default function MainScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.topBar}>
-        <Button title="Open Watchlists" onPress={() => setModalVisible(true)} />
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Crypto Watchlist</Text>
+        <Pressable style={styles.watchlistButton} onPress={() => setModalVisible(true)}>
+          <Ionicons name="list" size={24} color={Colors.cardBackground} />
+        </Pressable>
       </View>
 
       {selectedWatchlist ? (
-        <View style={styles.listArea}>
+        <View style={styles.content}>
           <View style={styles.watchlistHeader}>
-            <View style={styles.watchlistInfo}>
-              {/* TODO make shorter if too long */}
-              <Text style={styles.listTitle}>{selectedWatchlist.name}</Text>
+            <View style={styles.watchlistIconContainer}>
+              <Ionicons name={selectedWatchlist.icon as any} size={40} color={Colors.primary} />
             </View>
-            <Ionicons name={selectedWatchlist.icon as any} size={40} color="#8e44ad" />
-            <Pressable style={styles.shareIconContainer} onPress={handleShareWatchlist}>
-              <Ionicons name="share-social-outline" size={24} color="#8e44ad" />
+            <View style={styles.watchlistTitleContainer}>
+              <Text style={styles.watchlistTitle} numberOfLines={1}>
+                {selectedWatchlist.name}
+              </Text>
+            </View>
+            <Pressable style={styles.shareButton} onPress={handleShareWatchlist}>
+              <Ionicons name="share-social-outline" size={24} color={Colors.cardBackground} />
             </Pressable>
           </View>
 
@@ -119,14 +116,12 @@ export default function MainScreen() {
             data={displayedCoins}
             keyExtractor={(coin) => coin.id}
             renderItem={renderCoin}
-            ListEmptyComponent={
-              <Text style={{ textAlign: 'center', marginTop: 20 }}>No coin data found.</Text>
-            }
+            ListEmptyComponent={<Text style={styles.emptyText}>No coin data found.</Text>}
           />
         </View>
       ) : (
         <View style={styles.placeholder}>
-          <Text>Select a watchlist to see its coins & prices</Text>
+          <Text style={styles.placeholderText}>Select a watchlist to see its coins & prices</Text>
         </View>
       )}
 
@@ -143,61 +138,102 @@ export default function MainScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff', paddingTop: 20 },
-
-  topBar: {
-    paddingTop: 16,
+  container: {
+    flex: 1,
+    marginTop: '10%',
+    backgroundColor: Colors.background,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    marginBottom: 16,
+  },
+  headerTitle: {
+    color: Colors.primary,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  watchlistButton: {
+    backgroundColor: Colors.primary,
+    padding: 8,
+    borderRadius: 8,
+  },
+  content: {
+    flex: 1,
     paddingHorizontal: 16,
   },
-
-  listArea: {
-    flex: 1,
-    padding: 16,
-  },
-
-  listTitle: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginBottom: 12,
-    textAlign: 'center',
-  },
-
   watchlistHeader: {
+    flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 12,
+    backgroundColor: Colors.cardBackground,
+    padding: 12,
+    borderRadius: 12,
+    marginBottom: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
   },
-
+  watchlistIconContainer: {
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  watchlistTitleContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  watchlistTitle: {
+    fontSize: 22,
+    fontWeight: '600',
+    color: Colors.text,
+  },
+  shareButton: {
+    backgroundColor: Colors.secondary,
+    padding: 8,
+    borderRadius: 8,
+  },
+  coinRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: Colors.cardBackground,
+    padding: 12,
+    marginVertical: 6,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  coinName: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: Colors.text,
+  },
+  coinPrice: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: Colors.primary,
+  },
+  emptyText: {
+    textAlign: 'center',
+    marginTop: 20,
+    color: Colors.text,
+  },
   placeholder: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  coinRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 8,
-    borderBottomWidth: 0.5,
-    borderBottomColor: '#eee',
-  },
-
-  coinName: {
+  placeholderText: {
+    color: Colors.text,
     fontSize: 16,
-    fontWeight: '500',
-  },
-
-  coinPrice: {
-    fontSize: 16,
-    color: '#8e44ad',
-  },
-
-  watchlistInfo: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  shareIconContainer: {
-    marginLeft: 'auto',
-    padding: 8,
   },
 });
